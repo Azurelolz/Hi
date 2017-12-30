8f4d230b838448352846f80f5325e875266d2e69
Azurelolz <kai86310@gmail.com>
2017-12-30 17:26:30 +0800

    Initial Commit

---
+++ SA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SA
+//
+//  Created by 輔仁大學 on 2017/12/30.
+//  Copyright © 2017年 Seafood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
