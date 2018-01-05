3fe5084c832d9baeecdcc56f0ba60c79ef8a01c7
Jacob <haha110532@gmail.com>
2018-01-06 03:12:47 +0800

    改

--- SA/HotScheduleViewController.swift
+++ SA/HotScheduleViewController.swift
@@ -2,17 +2,14 @@
 //  HotScheduleViewController.swift
 //  SA
 //
-//  Created by 輔仁大學 on 2017/12/30.
-//  Copyright © 2017年 Seafood. All rights reserved.
+//  Created by 張昕暉 on 2018/1/6.
+//  Copyright © 2018年 Seafood. All rights reserved.
 //
 
 import UIKit
 
 class HotScheduleViewController: UIViewController {
 
-    
-    @IBOutlet var cityButtons: [UIButton]!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,34 +21,7 @@ class HotScheduleViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func handleSelection(_ sender: UIButton) {
-        cityButtons.forEach { (button) in
-            UIView.animate(withDuration: 0.3, animations: {
-                button.isHidden = !button.isHidden
-                self.view.layoutIfNeeded()
-            })
-        }
-    }
-    
-    enum Citys : String{
-        case place1 = "北部"
-        case place2 = "中部"
-        case place3 = "南部"
-        case place4 = "東部"
-    }
-    
-    @IBAction func cityTapped(_ sender: UIButton) {
-        guard let title = sender.currentTitle , let city = Citys(rawValue:title) else{
-            return
-            }
-        switch city{
-        case .place1 : print("北部")
-        case .place2 : print("中部")
-        case .place3 : print("南部")
-        case .place4 : print("東部")
-        }
-    }
-    
+
     /*
     // MARK: - Navigation
 
