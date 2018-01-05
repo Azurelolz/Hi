998e696f20f94b1b567909204aa89d3be574aee1
Jacob <haha110532@gmail.com>
2018-01-05 17:16:59 +0800

    熱門行程 選單

--- SA/HotScheduleViewController.swift
+++ SA/HotScheduleViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class HotScheduleViewController: UIViewController {
 
+    
+    @IBOutlet var cityButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +24,34 @@ class HotScheduleViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func handleSelection(_ sender: UIButton) {
+        cityButtons.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    enum Citys : String{
+        case place1 = "北部"
+        case place2 = "中部"
+        case place3 = "南部"
+        case place4 = "東部"
+    }
+    
+    @IBAction func cityTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle , let city = Citys(rawValue:title) else{
+            return
+            }
+        switch city{
+        case .place1 : print("北部")
+        case .place2 : print("中部")
+        case .place3 : print("南部")
+        case .place4 : print("東部")
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
