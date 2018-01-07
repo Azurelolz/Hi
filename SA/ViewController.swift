404dea0caa7c5204fd20c535ae6c65190fa4b823
Jacob <haha110532@gmail.com>
2018-01-07 17:39:35 +0800

    登入註冊完成

--- SA/ViewController.swift
+++ SA/ViewController.swift
@@ -17,45 +17,60 @@ class ViewController: UIViewController {
     @IBOutlet weak var actionButton: UIButton!
     
     @IBAction func action(_ sender: UIButton) {
+        if emailText.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
         if emailText.text != "" && passwordText.text != ""{
             if segmentControl.selectedSegmentIndex == 0 // login user
             {
                 Auth.auth().signIn(withEmail : emailText.text! , password : passwordText.text! , completion : { (user , error) in
-                    if user != nil
-                    {
-                        self.performSegue(withIdentifier: "segue", sender: self)
-                    }
-                    else
-                    {
-                        if let myError = error?.localizedDescription
-                        {
-                            print(myError)
-                        }
-                        else
-                        {
-                            print("Error")
-                        }
+                    if error == nil {
+                        
+                        // 登入成功，打印 ("You have successfully logged in")
+                        
+                        //Go to the HomeViewController if the login is sucessful
+                       self.performSegue(withIdentifier: "segue", sender: self)
+                        /*let vc = self.storyboard?.instantiateViewController(withIdentifier: "segue")
+                        self.present(vc!, animated: true, completion: nil)*/
+                        
+                    } else {
+                        
+                        // 提示用戶從 firebase 返回了一個錯誤。
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
                     }
                 })
             }
             else // Sing up user
             {
                 Auth.auth().createUser(withEmail : emailText.text! , password : passwordText.text! , completion : { (user , error) in
-                    if user != nil
-                    {
+                    if error == nil {
+                        print("You have successfully signed up")
+                        //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                        
                         self.performSegue(withIdentifier: "segue", sender: self)
+                        /*let vc = self.storyboard?.instantiateViewController(withIdentifier: "segue")
+                        self.present(vc!, animated: true, completion: nil)*/
+                        
+                    } else {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
                     }
-                    else
-                    {
-                        if let myError = error?.localizedDescription
-                        {
-                            print(myError)
-                        }
-                        else
-                        {
-                            print("Error")
-                        }
-                    }
+                    
                 })
             }
         }
