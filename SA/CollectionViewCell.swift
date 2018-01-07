3f9c34723862ea399d901f03eae6a85942c8db53
Azurelolz <kai86310@gmail.com>
2018-01-07 16:13:39 +0800

    nothing change

---
+++ SA/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  SA
+//
+//  Created by 輔仁大學 on 2018/1/7.
+//  Copyright © 2018年 Seafood. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var fireImageView: UIImageView!
+}
